e6c23f161eec476052ce35d3b89072ce291de00e
Vuonng Van Duy <vuongvanduyit03@gmail.com>
2024-07-26 16:48:14 +0700

    build MLKit

--- app/src/main/java/com/duyvv/camera/MainActivity.kt
+++ app/src/main/java/com/duyvv/camera/MainActivity.kt
@@ -1,27 +1,8 @@
 package com.duyvv.camera
 
-import android.Manifest
-import android.content.ContentValues
-import android.content.Intent
-import android.content.Intent.ACTION_VIEW
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCapture.OnImageSavedCallback
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.view.LifecycleCameraController
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.duyvv.camera.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,35 +14,4 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
-    /*private fun setupCamera() {
-        val preview = binding.preview
-        cameraController = LifecycleCameraController(baseContext)
-        cameraController.bindToLifecycle(this)
-        cameraController.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
-        preview.controller = cameraController
-
-        *//*val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-            .build()
-        val barcodeScanner = BarcodeScanning.getClient(options)
-
-        cameraController.setImageAnalysisAnalyzer(
-            ContextCompat.getMainExecutor(this),
-            MlKitAnalyzer(
-                listOf(barcodeScanner),
-                COORDINATE_SYSTEM_VIEW_REFERENCED,
-                ContextCompat.getMainExecutor(this)
-            ) { result: MlKitAnalyzer.Result? ->
-                val barcodes = result?.getValue(barcodeScanner)
-                if (!barcodes.isNullOrEmpty()) {
-                    barcodes[0].rawValue?.let {
-                        Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-                        startActivity(it)
-                        finish()
-                    }
-                }
-            }
-        )*//*
-    }*/
 }
--- app/src/main/java/com/duyvv/camera/ui/ScanQRFragment.kt
+++ app/src/main/java/com/duyvv/camera/ui/ScanQRFragment.kt
@@ -1,21 +1,30 @@
 package com.duyvv.camera.ui
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.COORDINATE_SYSTEM_ORIGINAL
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.mlkit.vision.MlKitAnalyzer
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.duyvv.camera.databinding.FragmentScanQrBinding
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 
 class ScanQRFragment : Fragment() {
 
@@ -54,11 +63,6 @@ class ScanQRFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         requestPermission()
-
-    }
-
-    private fun setup() {
-
     }
 
     private fun requestPermission() {
@@ -70,15 +74,16 @@ class ScanQRFragment : Fragment() {
     }
 
     private fun setupCamera() {
-        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext()).apply {
-            addListener(
-                {
-                    val cameraProvider = get()
-                    bindPreview(cameraProvider)
-                },
-                ContextCompat.getMainExecutor(requireContext())
-            )
-        }
+        cameraProviderFuture = ProcessCameraProvider
+            .getInstance(requireContext()).apply {
+                addListener(
+                    {
+                        val cameraProvider = get()
+                        bindPreview(cameraProvider)
+                    },
+                    ContextCompat.getMainExecutor(requireContext())
+                )
+            }
     }
 
     private fun bindPreview(cameraProvider: ProcessCameraProvider?) {
@@ -87,6 +92,50 @@ class ScanQRFragment : Fragment() {
             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
             .build()
         preview.surfaceProvider = binding.previewView.surfaceProvider
-        var camera = cameraProvider?.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+
+        // setup qr code scanner
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        val barcodeScanner = BarcodeScanning.getClient(options)
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER)
+            .build()
+
+        val mlKitAnalyzer = MlKitAnalyzer(
+            listOf(barcodeScanner),
+            COORDINATE_SYSTEM_ORIGINAL,
+            ContextCompat.getMainExecutor(requireContext())
+        ) { result: MlKitAnalyzer.Result? ->
+            val data = result?.getValue(barcodeScanner)
+            if (!data.isNullOrEmpty()) {
+                data[0].rawValue?.let {
+                    Log.d("TAG", it)
+                    Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                    startActivity(it)
+                    imageAnalysis.clearAnalyzer()
+                }
+            }
+        }
+
+        imageAnalysis.setAnalyzer(
+            ContextCompat.getMainExecutor(requireContext()),
+            mlKitAnalyzer
+        )
+
+        // set up camera
+        cameraProvider?.unbindAll()
+        cameraProvider?.bindToLifecycle(
+            viewLifecycleOwner,
+            cameraSelector,
+            imageAnalysis,
+            preview
+        )
+    }
+
+    private fun startActivity(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
